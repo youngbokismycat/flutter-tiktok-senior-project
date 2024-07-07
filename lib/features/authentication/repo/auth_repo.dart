e5fb545df1f868be20723c4531c4aca80cf6a3ab
Young Jeen <a01023620369@gmail.com>
2024-07-07 13:46:27 +0900

    move to mac

---
+++ lib/features/authentication/repo/auth_repo.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthRepository {
+  final FirebaseAuth _firebaseAuth;
+
+  AuthRepository(this._firebaseAuth);
+
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
+    await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future<void> signUpWithEmailAndPassword(String email, String password) async {
+    await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+
+  User? get currentUser => _firebaseAuth.currentUser;
+}
