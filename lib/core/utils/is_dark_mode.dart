5988345c17f99e16c527131c26fbee855a39b006
youngbok <a01023620369@gmail.com>
2024-07-09 12:57:00 +0900

    fix darkmmode

--- lib/core/utils/is_dark_mode.dart
+++ lib/core/utils/is_dark_mode.dart
@@ -1,11 +1,25 @@
 import 'dart:ui';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final isDarkModeProvider = Provider<bool>((ref) {
-  final brightness = PlatformDispatcher.instance.platformBrightness;
-  return brightness == Brightness.dark;
+class DarkModeNotifier extends StateNotifier<bool> {
+  DarkModeNotifier()
+      : super(
+            PlatformDispatcher.instance.platformBrightness == Brightness.dark) {
+    // Listen for platform brightness changes
+    PlatformDispatcher.instance.onPlatformBrightnessChanged = () {
+      // Update the state based on the new brightness
+      state = PlatformDispatcher.instance.platformBrightness == Brightness.dark;
+    };
+  }
+}
+
+// Provider for DarkModeNotifier
+final darkModeProvider = StateNotifierProvider<DarkModeNotifier, bool>((ref) {
+  return DarkModeNotifier();
 });
+
+// Function to get the current dark mode status
 bool isDarkMode(WidgetRef ref) {
-  final isDarkMode = ref.read(isDarkModeProvider);
+  final isDarkMode = ref.watch(darkModeProvider);
   return isDarkMode;
 }
