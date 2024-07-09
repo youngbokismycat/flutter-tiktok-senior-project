dbea83435d8706ef97876926e82641a6730dd693
youngbok <a01023620369@gmail.com>
2024-07-09 13:56:15 +0900

    darkmode stabilizing

--- lib/core/utils/is_dark_mode.dart
+++ lib/core/utils/is_dark_mode.dart
@@ -5,6 +5,7 @@ class DarkModeNotifier extends StateNotifier<bool> {
   DarkModeNotifier()
       : super(
             PlatformDispatcher.instance.platformBrightness == Brightness.dark) {
+    // Listen for platform brightness changes
     PlatformDispatcher.instance.onPlatformBrightnessChanged = () {
       // Update the state based on the new brightness
       state = PlatformDispatcher.instance.platformBrightness == Brightness.dark;
@@ -12,10 +13,12 @@ class DarkModeNotifier extends StateNotifier<bool> {
   }
 }
 
+// Provider for DarkModeNotifier
 final darkModeProvider = StateNotifierProvider<DarkModeNotifier, bool>((ref) {
   return DarkModeNotifier();
 });
 
+// Function to get the current dark mode status
 bool isDarkMode(WidgetRef ref) {
   final isDarkMode = ref.watch(darkModeProvider);
   return isDarkMode;
