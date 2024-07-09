7ad5999d183f2421c6604f286f310b4d2b0fdd0c
Young Jeen <a01023620369@gmail.com>
2024-07-09 17:44:58 +0900

    toss to mac

--- lib/features/home/view_my_dairy_screen.dart
+++ lib/features/home/view_my_dairy_screen.dart
@@ -5,8 +5,8 @@ import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_senior_project/core/utils/is_dark_mode.dart';
-import 'package:flutter_senior_project/features/add/add_my_dairy_screen.dart';
 import 'package:flutter_senior_project/features/common/widget/custom_shader.dart';
+import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ViewMyDairyScreen extends ConsumerStatefulWidget {
@@ -24,41 +24,26 @@ class ViewMyDairyScreenState extends ConsumerState<ViewMyDairyScreen> {
       title: const CustomShader(
         child: Text(
           "물들다",
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
       ),
-      headerWidget: headerWidget(context),
-      headerBottomBar: headerBottomBarWidget(),
-      body: [
-        listView(),
-      ],
-    );
-  }
-
-  Row headerBottomBarWidget() {
-    return const Row(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.end,
-      crossAxisAlignment: CrossAxisAlignment.center,
-    );
-  }
-
-  Widget headerWidget(BuildContext context) {
-    return AnimateGradient(
-      duration: const Duration(seconds: 20),
-      primaryBeginGeometry: const AlignmentDirectional(0, 3),
-      primaryEndGeometry: const AlignmentDirectional(0, 2),
-      secondaryBeginGeometry: const AlignmentDirectional(2, 0),
-      secondaryEndGeometry: const AlignmentDirectional(0, -0.8),
-      textDirectionForGeometry: TextDirection.rtl,
-      primaryColors: const [
-        FlexColor.blueDarkPrimary,
-        FlexColor.redWineDarkPrimary,
-      ],
-      secondaryColors: const [
-        FlexColor.redWineDarkPrimary,
-        FlexColor.blueDarkPrimary,
-      ],
-      child: Container(
+      headerWidget: AnimateGradient(
+        duration: const Duration(seconds: 20),
+        primaryBeginGeometry: const AlignmentDirectional(0, 3),
+        primaryEndGeometry: const AlignmentDirectional(0, 2),
+        secondaryBeginGeometry: const AlignmentDirectional(2, 0),
+        secondaryEndGeometry: const AlignmentDirectional(0, -0.8),
+        textDirectionForGeometry: TextDirection.rtl,
+        primaryColors: const [
+          FlexColor.blueDarkPrimary,
+          FlexColor.redWineDarkPrimary,
+        ],
+        secondaryColors: const [
+          FlexColor.redWineDarkPrimary,
+          FlexColor.blueDarkPrimary,
+        ],
         child: Center(
           child: Text(
             "물들다",
@@ -69,24 +54,36 @@ class ViewMyDairyScreenState extends ConsumerState<ViewMyDairyScreen> {
           ),
         ),
       ),
-    );
-  }
-
-  ListView listView() {
-    return ListView.builder(
-      padding: const EdgeInsets.only(top: 0),
-      physics: const NeverScrollableScrollPhysics(),
-      itemCount: 20,
-      shrinkWrap: true,
-      itemBuilder: (context, index) => Card(
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Text("$index"),
-          ),
-          title: const Text("Title"),
-          subtitle: const Text("Subtitle"),
-        ),
+      headerBottomBar: const Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
       ),
+      body: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: ListView.separated(
+            padding: const EdgeInsets.only(top: 0),
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: 20,
+            shrinkWrap: true,
+            separatorBuilder: (context, index) => const Gap(10),
+            itemBuilder: (context, index) => Container(
+              height: 200,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    24,
+                  ),
+                  border: Border.all(
+                    width: 1,
+                  )),
+              child: Text(
+                faker.lorem.sentence(),
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
