0fdc0627dd5610177d3b4c5e533a3eb50ee48c07
youngbok <a01023620369@gmail.com>
2024-07-10 15:51:17 +0900

    implement darkmode of detail diary screen and add go back feature

--- lib/features/common/widget/custom_animate_gradient.dart
+++ lib/features/common/widget/custom_animate_gradient.dart
@@ -1,8 +1,10 @@
 import 'package:animate_gradient/animate_gradient.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_senior_project/core/utils/is_dark_mode.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class CustomAnimateGradient extends StatelessWidget {
+class CustomAnimateGradient extends ConsumerWidget {
   const CustomAnimateGradient({
     super.key,
     required this.child,
@@ -10,23 +12,25 @@ class CustomAnimateGradient extends StatelessWidget {
   final Widget child;
 
   @override
-  Widget build(BuildContext context) {
-    return AnimateGradient(
-      duration: const Duration(seconds: 20),
-      primaryBeginGeometry: const AlignmentDirectional(0, 3),
-      primaryEndGeometry: const AlignmentDirectional(0, 2),
-      secondaryBeginGeometry: const AlignmentDirectional(2, 0),
-      secondaryEndGeometry: const AlignmentDirectional(0, -0.8),
-      textDirectionForGeometry: TextDirection.rtl,
-      primaryColors: const [
-        FlexColor.blueDarkPrimary,
-        FlexColor.redWineDarkPrimary,
-      ],
-      secondaryColors: const [
-        FlexColor.redWineDarkPrimary,
-        FlexColor.blueDarkPrimary,
-      ],
-      child: child,
-    );
+  Widget build(BuildContext context, WidgetRef ref) {
+    return isDarkMode(ref)
+        ? child
+        : AnimateGradient(
+            duration: const Duration(seconds: 20),
+            primaryBeginGeometry: const AlignmentDirectional(0, 3),
+            primaryEndGeometry: const AlignmentDirectional(0, 2),
+            secondaryBeginGeometry: const AlignmentDirectional(2, 0),
+            secondaryEndGeometry: const AlignmentDirectional(0, -0.8),
+            textDirectionForGeometry: TextDirection.rtl,
+            primaryColors: const [
+              FlexColor.blueDarkPrimary,
+              FlexColor.redWineDarkPrimary,
+            ],
+            secondaryColors: const [
+              FlexColor.redWineDarkPrimary,
+              FlexColor.blueDarkPrimary,
+            ],
+            child: child,
+          );
   }
 }
--- lib/features/detail_diary/detail_diary_card_screen.dart
+++ lib/features/detail_diary/detail_diary_card_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_senior_project/features/common/widget/custom_animate_gradient.dart';
 import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:flutter_senior_project/core/config/wheather_config.dart';
@@ -24,70 +25,75 @@ class DetailDiaryCardScreen extends HookConsumerWidget {
       opacity.value = 1.0;
     });
 
-    return Scaffold(
-      body: CustomAnimateGradient(
-        child: Center(
-          child: Hero(
-            tag: '$index',
-            child: Transform.translate(
-              offset: const Offset(
-                0,
-                15,
-              ),
-              child: Material(
-                color: Colors.transparent,
-                child: Stack(
-                  alignment: Alignment.topCenter,
-                  children: [
-                    Transform.scale(
-                      scale: 1.1,
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.circular(24),
-                          child: WeatherConfiguration.rainyEvening),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 25.0,
+    return GestureDetector(
+      onTap: () => context.pop(),
+      child: Scaffold(
+        body: CustomAnimateGradient(
+          child: Center(
+            child: Hero(
+              tag: '$index',
+              child: Transform.translate(
+                offset: const Offset(
+                  0,
+                  15,
+                ),
+                child: Material(
+                  color: Colors.transparent,
+                  child: Stack(
+                    alignment: Alignment.topCenter,
+                    children: [
+                      Transform.scale(
+                        scale: 1.1,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(24),
+                            child: WeatherConfiguration.rainyEvening),
                       ),
-                      child: ListTile(
-                        leading: AnimatedEmoji(
-                          AnimatedEmojis.sad,
-                          size: 32,
+                      const Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 25.0,
                         ),
-                        title: Text(
-                          '오늘 너무 슬펐다.',
-                          style: TextStyle(
-                            color: Colors.white,
+                        child: ListTile(
+                          leading: AnimatedEmoji(
+                            AnimatedEmojis.sad,
+                            size: 32,
                           ),
-                        ),
-                        subtitle: Text(
-                          '그냥 슬펐다.',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Transform.translate(
-                      offset: const Offset(0, 80),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 30,
-                        ),
-                        child: AnimatedOpacity(
-                          opacity: opacity.value,
-                          duration: const Duration(seconds: 1),
-                          child: const Text(
-                            '아침부터 기분이 안 좋았다. 날씨도 우중충하고, 머리도 띵해서 공부한 내용이 머릿속에 제대로 들어오지도 않았다. 시험지 앞에 앉았을 때, 문제들이 너무 어려워서 당황했다. 분명 공부했던 내용인데, 머릿속이 하얘졌다. 첫 문제부터 막혔고, 시간이 갈수록 더 조급해졌다. 시계 초침 소리가 점점 크게 들리는 것 같았다. 답을 적으려고 애썼지만, 자신이 없었다. 마지막 문제까지 제대로 풀지 못했다.',
+                          title: Text(
+                            '오늘 너무 슬펐다.',
                             style: TextStyle(
                               color: Colors.white,
-                              fontSize: 20,
+                            ),
+                          ),
+                          subtitle: Text(
+                            '그냥 슬펐다.',
+                            style: TextStyle(
+                              color: Colors.white,
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                      Transform.translate(
+                        offset: const Offset(0, 80),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 25,
+                          ),
+                          child: AnimatedOpacity(
+                            opacity: opacity.value,
+                            duration: const Duration(seconds: 1),
+                            child: const ListTile(
+                              title: Text(
+                                '아침부터 기분이 안 좋았다. 날씨도 우중충하고, 머리도 띵해서 공부한 내용이 머릿속에 제대로 들어오지도 않았다. 시험지 앞에 앉았을 때, 문제들이 너무 어려워서 당황했다. 분명 공부했던 내용인데, 머릿속이 하얘졌다. 첫 문제부터 막혔고, 시간이 갈수록 더 조급해졌다. 시계 초침 소리가 점점 크게 들리는 것 같았다. 답을 적으려고 애썼지만, 자신이 없었다. 마지막 문제까지 제대로 풀지 못했다.',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
