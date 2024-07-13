e8fef076e23e98837976a608ef27cbf7171588e6
Young Jeen <a01023620369@gmail.com>
2024-07-13 22:14:12 +0900

    bug fix - unexpected initialization of gradiant effect

--- lib/features/common/widget/custom_animate_gradient.dart
+++ lib/features/common/widget/custom_animate_gradient.dart
@@ -1,10 +1,11 @@
 import 'package:animate_gradient/animate_gradient.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_senior_project/features/common/utils/is_dark_mode.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class CustomAnimateGradient extends ConsumerWidget {
+class CustomAnimateGradient extends ConsumerStatefulWidget {
   const CustomAnimateGradient({
     super.key,
     required this.child,
@@ -12,10 +13,37 @@ class CustomAnimateGradient extends ConsumerWidget {
   final Widget child;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  CustomAnimateGradientState createState() => CustomAnimateGradientState();
+}
+
+class CustomAnimateGradientState extends ConsumerState<CustomAnimateGradient>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController animationControllerForGradient;
+  @override
+  void initState() {
+    super.initState();
+    // Assign the controller
+    animationControllerForGradient = (AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 10, //duration you need
+      ),
+    )..repeat(reverse: true //false for restarting from the begining
+        ));
+  }
+
+  @override
+  void dispose() {
+    animationControllerForGradient.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return isDarkMode(ref)
-        ? child
+        ? widget.child
         : AnimateGradient(
+            controller: animationControllerForGradient,
             duration: const Duration(seconds: 20),
             primaryBeginGeometry: const AlignmentDirectional(0, 3),
             primaryEndGeometry: const AlignmentDirectional(0, 2),
@@ -30,7 +58,7 @@ class CustomAnimateGradient extends ConsumerWidget {
               FlexColor.redWineDarkPrimary,
               FlexColor.blueDarkPrimary,
             ],
-            child: child,
+            child: widget.child,
           );
   }
 }
